3da85a4d3a0f8afdbe41fd0c77acd1e690715e89
Michael Librodo <librodo@tatari.tv>
2022-07-11 14:14:40 +0800

    some config management

--- config/config.go
+++ config/config.go
@@ -1,58 +1,21 @@
 package config
 
-import (
-	"time"
-
-	"github.com/spf13/viper"
-)
-
-// Provider defines a set of read-only methods for accessing the application
-// configuration params as defined in one of the config files.
-type Provider interface {
-	ConfigFileUsed() string
-	Get(key string) interface{}
-	GetBool(key string) bool
-	GetDuration(key string) time.Duration
-	GetFloat64(key string) float64
-	GetInt(key string) int
-	GetInt64(key string) int64
-	GetSizeInBytes(key string) uint
-	GetString(key string) string
-	GetStringMap(key string) map[string]interface{}
-	GetStringMapString(key string) map[string]string
-	GetStringMapStringSlice(key string) map[string][]string
-	GetStringSlice(key string) []string
-	GetTime(key string) time.Time
-	InConfig(key string) bool
-	IsSet(key string) bool
+type Config struct {
+	Logging LoggingConfig `mapstructure:"logging"`
+	AWS     AWSConfig     `mapstructure:"aws"`
+	Backup  BackupConfig  `mapstructure:"backup"`
 }
 
-var defaultConfig *viper.Viper
-
-// Config returns a default config providers
-func Config() Provider {
-	return defaultConfig
+type BackupConfig struct {
+	S3Bucket string `mapstructure:"s3_bucket"`
 }
 
-// LoadConfigProvider returns a configured viper instance
-func LoadConfigProvider(appName string) Provider {
-	return readViperConfig(appName)
+type AWSConfig struct {
+	ServiceConfigProfile string `mapstructure:"serviceConfigProfile"`
+	AccountId            string `mapstructure:"accountId"`
 }
 
-func init() {
-	defaultConfig = readViperConfig("DB-COPIER")
-}
-
-func readViperConfig(appName string) *viper.Viper {
-	v := viper.New()
-	v.SetEnvPrefix(appName)
-	v.AutomaticEnv()
-
-	// global defaults
-	
-	v.SetDefault("json_logs", false)
-	v.SetDefault("loglevel", "debug")
-	
-
-	return v
+type LoggingConfig struct {
+	Level   string `mapstructure:"level"`
+	JSONLog bool   `mapstructure:"jsonLogs"`
 }
